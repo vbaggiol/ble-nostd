4f2ce1f9652b1c4c47fe4ab25669eb1be0091957
Vito <vbaggiol@gmail.com>
2025-02-01 12:21:03 +0100

    With BLE advertising (adapted wifi_ble.rs)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,27 +1,81 @@
+//! BLE Example https://github.com/esp-rs/esp-hal/blob/main/examples/src/bin/wifi_ble.rs
+//!
+//! - starts Bluetooth advertising
+//! - offers one service with three characteristics (one is read/write, one is write only, one is read/write/notify)
+//! - pressing the boot-button on a dev-board will send a notification if it is subscribed
+
+//% FEATURES: esp-wifi esp-wifi/ble esp-hal/unstable
+//% CHIPS: esp32 esp32s3 esp32c2 esp32c3 esp32c6 esp32h2
+
 #![no_std]
 #![no_main]
 
-// You'll need a panic handler e.g. `use esp_backtrace as _;`
+use bleps::{
+    ad_structure::{
+        create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
+    },
+    att::Uuid,
+    Ble, HciConnector,
+};
+use esp_alloc as _;
+use esp_backtrace as _;
 use esp_hal::{
     clock::CpuClock,
     delay::Delay,
-    gpio::{Level, Output},
     main,
+    rng::Rng,
+    time,
+    timer::timg::TimerGroup,
 };
-use esp_backtrace as _;
+use esp_println::println;
+use esp_wifi::{ble::controller::BleConnector, init};
 
 #[main]
 fn main() -> ! {
+    esp_println::logger::init_logger_from_env();
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    // Set GPIO10 as an output, and set its state high initially.
-    let mut led = Output::new(peripherals.GPIO10, Level::High);
+    esp_alloc::heap_allocator!(72 * 1024);
 
-    let delay = Delay::new();
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
 
+    let init = init(
+        timg0.timer0,
+        Rng::new(peripherals.RNG),
+        peripherals.RADIO_CLK,
+    )
+    .unwrap();
+
+    let mut bluetooth = peripherals.BT;
+
+    let now = || time::now().duration_since_epoch().to_millis();
     loop {
-        led.toggle();
-        delay.delay_millis(300);
+        let connector = BleConnector::new(&init, &mut bluetooth);
+        let hci = HciConnector::new(connector, now);
+        let mut ble = Ble::new(&hci);
+
+        println!("{:?}", ble.init());
+        println!("{:?}", ble.cmd_set_le_advertising_parameters());
+        println!(
+            "{:?}",
+            ble.cmd_set_le_advertising_data(
+                create_advertising_data(&[
+                    AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+                    AdStructure::ServiceUuids16(&[Uuid::Uuid16(0x1809)]),
+                    AdStructure::CompleteLocalName("BeaconX32"),
+                ])
+                .unwrap()
+            )
+        );
+        println!("{:?}", ble.cmd_set_le_advertise_enable(true));
+
+        println!("started advertising");
+
+        let delay = Delay::new();
+
+        loop {
+            delay.delay_millis(50);
+        }
     }
 }
